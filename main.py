8594aa28803a8cb81478cd3fcfd5adbec1726bda
Эдуард <alexed34@yandex.ru>
2020-01-26 12:43:19 +0400

    Изменил структуру кода
    добавил if main
    и разбил код на функции

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # coding=utf-8
-
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import os
@@ -10,31 +8,19 @@ import sys
 import argparse
 
 
-
-load_dotenv()
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-path_file_wine = os.getenv('FILE_WINE')
-
-
-def createParser():
+def createParser(path_file_wine):
     parser = argparse.ArgumentParser()
     parser.add_argument('name', nargs='?', default=path_file_wine)
     return parser
 
 
-parser = createParser()
-namespace = parser.parse_args(sys.argv[1:])
+def read_file(filname):
+    with open(filname.name, 'r', encoding='utf-8') as f:
+        text = f.read()
+        return text
 
 
-with open(namespace.name, 'r', encoding='utf-8') as f:
-    text = f.read()
+def convert_text(text):
     text_fragments = text.split('#')
     all_wines = []
     for fragment in text_fragments[1:]:
@@ -52,16 +38,41 @@ with open(namespace.name, 'r', encoding='utf-8') as f:
             wine_category['wine'] = wines
         all_wines.append(wine_category)
     print(all_wines)
+    return all_wines
 
 
-rendered_page = template.render(
-    data_year=datetime.datetime.now().year - 1920,
-    wines=all_wines,
 
-)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    load_dotenv()
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    path_file_wine = os.getenv('FILE_WINE')
+    parser = createParser(path_file_wine)
+    namespace = parser.parse_args(sys.argv[1:])
+
+    text = read_file(namespace)
+    all_wines = convert_text(text)
+
+
+    rendered_page = template.render(
+        data_year=datetime.datetime.now().year - 1920,
+        wines=all_wines,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
