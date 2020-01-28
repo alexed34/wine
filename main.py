748a179d2c53e95dd2e18c01dd9aa8353287994b
Эдуард <alexed34@yandex.ru>
2020-01-28 21:35:02 +0400

    Исправил замечания 16-25

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
@@ -8,9 +7,9 @@ import sys
 import argparse
 
 
-def get_file_terminal(path_file_wine):
+def get_command_line_argument(path_wine_file):
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', nargs='?', default=path_file_wine)
+    parser.add_argument('-n', '--name', default=path_wine_file)
     namespace = parser.parse_args(sys.argv[1:])
     return namespace
 
@@ -25,52 +24,22 @@ def convert_text(text):
     text_fragments = text.split('#')
     all_wines = []
     for fragment in text_fragments[1:]:
-        wine_category = {}
-        wine_category['name'] = fragment.strip().split('\n\n')[0]
-        wines_in_fragment = fragment.strip().split('\n\n')[1:]
+        category_fragment = fragment.strip().split('\n\n')
+        wine_category = {
+            'name': category_fragment[0]
+        }
+        wines_in_category = category_fragment[1:]
         wines = []
-        for wine in wines_in_fragment:
+        for wine in wines_in_category:
             wine_description = wine.strip().split('\n')
             list_wine_characteristics = []
             for characteristics in wine_description:
                 characteristics = characteristics.split()
                 list_wine_characteristics.append(' '.join(characteristics[1:]))
             wines.append(list_wine_characteristics)
-            wine_category['wine'] = wines
+            wine_category['wines'] = wines
         all_wines.append(wine_category)
-    print(all_wines)
     return all_wines
-
-# Альтернативная версия конвертации текста
-# def convert_text(text):
-#     text = text.split('\n')
-#     all_wines = []
-#     for i in text:
-#         if '#' in i:
-#             wine_category = {}
-#             wine_category['name'] = i.strip('#, , \n')
-#             all_wines.append(wine_category)
-#             wines = []
-#             wine_category['wine'] = wines
-#         elif 'Название' in i:
-#             list_wine_characteristics = []
-#             wines.append(list_wine_characteristics)
-#             name = i.split(':')
-#             list_wine_characteristics.append(name[1].strip())
-#         elif 'Сорт' in i:
-#             name = i.split(':')
-#             list_wine_characteristics.append(name[1].strip())
-#         elif 'Цена' in i:
-#             name = i.split(':')
-#             list_wine_characteristics.append(name[1].strip())
-#         elif 'Картинка' in i:
-#             name = i.split(':')
-#             list_wine_characteristics.append(name[1].strip())
-#         elif 'Выгодное' in i:
-#             name = i.split(' ')
-#             list_wine_characteristics.append(name[1].strip())
-#     print(all_wines)
-#     return all_wines
 
 
 def render_page(env, all_wines):
@@ -89,9 +58,8 @@ def main():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
-    path_file_wine = os.getenv('FILE_WINE')
-    namespace = get_file_terminal(path_file_wine)
+    path_wine_file = os.getenv('WINES_FILE')
+    namespace = get_command_line_argument(path_wine_file)
     text = read_file(namespace)
     all_wines = convert_text(text)
     render_page(env, all_wines)
